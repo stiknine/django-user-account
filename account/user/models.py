2ffcc2d887b1152e4398544f0ef0d4924494f9b0
Harley <stiknine@gmail.com>
2023-02-16 16:40:53 -0700

    update user account email field to be unique

--- account/user/models.py
+++ account/user/models.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy
 
 
 class Account(AbstractUser):
     """Custom user account model.
 
-    A subclass of Django's AbstractUser class. It overrides one method.
+    A subclass of Django's AbstractUser class. It overrides one method and updates email to be a unique field.
     A custom User model is the recommended way if using Django's user authentication system.
     """
+
+    email = models.EmailField(
+        gettext_lazy("email address"),
+        unique=True,
+        error_messages={
+            "unique": gettext_lazy("A user with that email already exists."),
+        },
+    )
 
     def __str__(self):
         return self.username
